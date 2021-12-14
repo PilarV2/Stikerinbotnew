1af62115daffc1d44b85e3beed42938e885cbf4f
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2021-12-14 15:46:13 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,18 +1,8 @@
 let fetch = require('node-fetch')
+let donasi = global.donate
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-╭─「  Donasi • Pulsa 」
-├❒ tre [+62 895-3362-82144]
-╰────
-
-╭─「  Donasi • Non Pulsa 」
-├❒ Gopay [0895336282144]
-├❒ saweria [https://saweria.co/thesadboy01]
-╰────
-
-╭─「  Hubungi 」
-├❒ Ingin donasi? Wa.me/62895336282144
-╰────
-`.trim(), 'Yang donasi dapat pahala', 'Donasi', '.donasi', m)
+${donasi}
+`.trim(), 'Yang donasi dapat pahala', 'Owner', '.owner', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
