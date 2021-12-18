3c5e36dfc2c6da88b5c17e9447eb3ee70488d480
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2021-12-18 07:27:12 +0700

    Update gcbot.js

--- plugins/gcbot.js
+++ plugins/gcbot.js
@@ -2,18 +2,11 @@ let handler = async m => m.reply(`
 ◇ *⚠GRUP BOT⚠*
 
 
-① https://chat.whatsapp.com/CnQV9lNcHuIENb8SQ9CaYP
+《 officiall Group *${conn.getName(conn.user.jid)}* 》 :
+${(global.linkGC).map((v, i) => '➥ *Group ' + (i + 1) + '*\n' + v).join`\n \n`}
 
-② https://chat.whatsapp.com/Hjr3H7l9iC8F2deP0cv6GT
-
-③ https://chat.whatsapp.com/CkJChTj3OaSGnWvryR3i54
-
-④ https://chat.whatsapp.com/Lb4Emjih98rBiCZiZoS2eM
-
-⑤ https://chat.whatsapp.com/FrluVyY5kBRIhY6yKfH4DI
-
-*《DIAHARAPKAN UNTUK MEMATUHI PERATURAN GRUP ITU》*
-`.trim()) // Tambah sendiri kalo mau
+*《 DIAHARAPKAN UNTUK MEMATUHI PERATURAN GRUP ITU 》*
+`.trim())
 handler.help = ['gcbot']
 handler.tags = ['info']
 handler.command = /^gcbot$/i
