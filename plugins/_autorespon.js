b97de60d4d864095675fdc3bd7fecbbe320199b0
Zeus Botz <97302454+PilarV2@users.noreply.github.com>
2022-02-23 08:36:18 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -14,7 +14,7 @@ handler.all = async function (m, { isBlocked }) {
     try {
         if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
             await this.send2Button(m.chat,
-                isBanned ? 'Stikerinbotnew tidak aktif' : banned ? 'kamu dibanned' : 'Stikerinbotnew bot disini',
+                isBanned ? 'Stikerinbotnew tidak aktif' : banned ? 'kamu dibanned' : 'Bot Wangsaf bot disini',
                 wm,
                 isBanned ? 'Unban' : banned ? 'Pemilik Bot' : 'Menu',
                 isBanned ? '.unban' : banned ? '.owner' : '.menu',
@@ -33,17 +33,9 @@ handler.all = async function (m, { isBlocked }) {
 ├ 30 Hari / Rp 15,000
 └────
 
-https://github.com/Kangsad01/Stikerinbotnew
+https://github.com/PilarV2
 `.trim(), wm, 'Pemilik Bot', ',owner', m)
     }
-
-    // salam
-    let reg = /(ass?alam|اَلسَّلاَمُ عَلَيْكُمْ|السلام عليکم)/i
-    let isSalam = reg.exec(m.text)
-    if (isSalam && !m.fromMe) {
-        m.reply(`وَعَلَيْكُمْ السَّلاَمُ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ\n_wa\'alaikumussalam wr.wb._`)
-    }
-
     // backup db
     if (setting.backup) {
         if (new Date() * 1 - setting.backupDB > 1000 * 60 * 60) {
@@ -65,7 +57,7 @@ https://github.com/Kangsad01/Stikerinbotnew
         let _uptime = process.uptime() * 1000
         let uptime = clockString(_uptime)
         totaluser = Object.keys(db.data.users)
-        await this.setStatus(`Aktif selama ${uptime} │ Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} │ total user : ${totaluser.length} │ Stikerinbot new`).catch(_ => _)
+        await this.setStatus(`Aktif selama ${uptime} │ Mode: ${global.opts['self'] ? 'Private' : setting.groupOnly ? 'Hanya Grup' : 'Publik'} │ total user : ${totaluser.length} │ ZeusBotzNew`).catch(_ => _)
         setting.status = new Date() * 1
     }
 
